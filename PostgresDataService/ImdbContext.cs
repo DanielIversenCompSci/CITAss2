6425e21838b645f377777c3d2e1cd7d7ad99551c
danieliversen <daniel.emil.iversen@gmail.com>
2024-10-28 13:22:02 +0100

    Implementation of first domain object

--- PostgresDataService/ImdbContext.cs
+++ PostgresDataService/ImdbContext.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//Db context
 namespace PostgresDataService;
 internal class ImdbContext : DbContext
 {
