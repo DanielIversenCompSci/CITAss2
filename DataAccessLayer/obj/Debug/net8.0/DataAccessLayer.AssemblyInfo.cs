c30c71c99b1b035a92a71a106962a0e7faf21443
jonathanLOlsen <jonathan.laage.olsen@gmail.com>
2024-10-30 13:21:09 +0100

    added namebasic

--- DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
+++ DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1bffe1253b96db5b1181ad0123117b4346684c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9579ce3a07d0c429c5e8fb2373ca63bb68a938d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
