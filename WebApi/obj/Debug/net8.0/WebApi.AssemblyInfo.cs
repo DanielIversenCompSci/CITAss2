c7ac8c33f11a6f5e45176b8b46b528c50d367e95
jonathanLOlsen <jonathan.laage.olsen@gmail.com>
2024-11-03 20:18:20 +0100

    added controller for titlebasics

--- WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f10acbd80daa2759d75149243fbef91ca807e08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c17607e5a60b87bea373d60a416ce0d4dca70e1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
