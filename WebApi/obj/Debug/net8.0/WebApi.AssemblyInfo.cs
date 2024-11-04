24aa12b9c7ae4be71e3dfe24cc48cbbb693f90da
AlfredSchou <abjarneg@live.dk>
2024-11-04 13:02:28 +0100

    git pull recent

--- WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dec0fcfed3eb96ce61bc6ae6196eabf37d75ca3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17c6a23627252fcba4f3ffc291a1033e80de7c05")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
