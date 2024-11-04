26507638b1ff6710a761dda4c8d22a1119846705
AlfredSchou <abjarneg@live.dk>
2024-11-04 14:14:30 +0100

    added KnownForTitle Controller + changed composite key for knownForTitle mapping

---
+++ WebApi/Controllers/KnownForTitleController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BusinessLayer;
+using DataAccessLayer;
+using System.Collections.Generic;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KnownForTitleController : ControllerBase
+    {
+        private readonly IDataService _dataService;
+
+        public KnownForTitleController(IDataService dataService)
+        {
+            _dataService = dataService;
+        }
+
+        
+        [HttpGet]
+        public ActionResult<IEnumerable<KnownForTitle>>GetKnownForTitleList()
+        {
+            var knownForTitlelist = _dataService.GetKnownForTitleList();
+            return Ok(knownForTitlelist);
+        }
+
+
+        [HttpGet("{tConst}/{nConst}")]
+        public ActionResult<KnownForTitle> GetKnownForTitleById(string tConst, string nConst)
+        {
+            var title = _dataService.GetKnownForTitleById(tConst, nConst);
+
+            if (title == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(title);
+        }
+
+
+        [HttpPost]
+        public ActionResult<KnownForTitle> CreateKnownForTitle([FromBody] KnownForTitle newTitle)
+        {
+            var createdTitle = _dataService.AddKnownForTitle(newTitle);
+
+            return CreatedAtAction(nameof(GetKnownForTitleById), new { userId = createdTitle.TConst, timestamp = createdTitle.NConst}, createdT);
+        }
+
+
+        [HttpPut("{tConst}/{nConst}")]
+        public ActionResult<KnownForTitle> UpdateKnownForTitle(string tConst, string nConst, [FromBody] KnownForTitle updatedTitle)
+        {
+            var success = _dataService.UpdateKnownForTitle(tConst, nConst, updatedTitle);
+
+            if (!success)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{tConst}/{nConst}")]
+        public IActionResult DeleteKnownForTitle(string tConst, string nConst)
+        {
+            var success = _dataService.DeleteKnownForTitle(tConst, nConst);
+
+            if (!success)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+    }
+}
--- WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17c6a23627252fcba4f3ffc291a1033e80de7c05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24aa12b9c7ae4be71e3dfe24cc48cbbb693f90da")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
