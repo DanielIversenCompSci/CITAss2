b1d626dda33a9668f8fb42c4bf8baf3d4625bfa5
danieliversen <daniel.emil.iversen@gmail.com>
2024-12-04 10:55:21 +0100

    .

--- WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e32727cdc411b1e7633452a6a0856c08e31c092b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+007d94b341843642d72bad72f27e0b1d57fadfb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
