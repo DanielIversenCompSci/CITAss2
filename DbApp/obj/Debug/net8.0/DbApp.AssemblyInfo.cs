b91cbcd84a2b7ec1d7e2634fd714342f7643932a
danieliversen <daniel.emil.iversen@gmail.com>
2024-12-11 21:26:43 +0100

    Added DTOS for specific endpoints data

--- DbApp/obj/Debug/net8.0/DbApp.AssemblyInfo.cs
+++ DbApp/obj/Debug/net8.0/DbApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DbApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71aa2b737ccf1089a72072e117d55d2f6fa78d0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bf73cf7a7d1c9e5c92fb23d70d4c60efe1c4dcd")]
 [assembly: System.Reflection.AssemblyProductAttribute("DbApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DbApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71aa2b737ccf1089a72072e117d55d2f6fa78d0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bf73cf7a7d1c9e5c92fb23d70d4c60efe1c4dcd")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
