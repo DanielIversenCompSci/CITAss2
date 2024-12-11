ebec0d8c3a965956aa3c7e08951140e8d191ae4c
AlfredSchou <abjarneg@live.dk>
2024-12-11 11:36:54 +0100

    123

--- WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dcebaeec3d2a8dae75c0f063301eb4facb71d3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb2d7e76dc4fc57d49470bf9b67123ad066a4c30")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
