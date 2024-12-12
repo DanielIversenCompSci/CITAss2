1aaf3177ee2525bd31c3a97ffc04596bf665d781
AlfredSchou <abjarneg@live.dk>
2024-12-12 13:27:32 +0100

    FIXED

--- WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+085f05a8d50fa89cce9dedc12354a036ad5cc12f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0467a1bd11ed437b0d90f6fe218bf02d107d560f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
