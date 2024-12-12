9d1fc23f6288bc1c9879a20292f8c8ff737bc430
danieliversen <daniel.emil.iversen@gmail.com>
2024-12-12 11:13:03 +0100

    .

--- DbApp/obj/Debug/net8.0/DbApp.AssemblyInfo.cs
+++ DbApp/obj/Debug/net8.0/DbApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DbApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38af4dc5465045be2027e82ea4a46c0143697e7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea28ea471bd1f952639d922b90cb9a42b8836886")]
 [assembly: System.Reflection.AssemblyProductAttribute("DbApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DbApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
